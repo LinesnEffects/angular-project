40d430d82797c6f9d2753534349317070b28168c
LinesnEffects <bgf2311@outlook.com>
2025-01-13 11:24:32 -0500

    Limited add component to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./components/header/header.component";
-import { TasksComponent } from "./components/tasks/tasks.component";
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, TasksComponent, RouterOutlet, FooterComponent],
+  imports: [CommonModule, HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
